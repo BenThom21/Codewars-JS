4440451c9b872ca9062f6ae6ceec1a3d1813da75
Ben Thompson <benthompson4496@gmail.com>
2018-09-16 17:37:28 -0500

    Update two-fighers_one-winner.js

--- two-fighers_one-winner.js
+++ two-fighers_one-winner.js
@@ -2,6 +2,7 @@ function Fighter(name, health, damagePerAttack) {
   this.name = name;
   this.health = health;
   this.damagePerAttack = damagePerAttack;
+  this.toString = function() {return this.name;}
 }
 
 function declareWinner(fighter1, fighter2, firstAttacker) {
